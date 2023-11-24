678b35f9b49e7db7a075b01e55059666d38f2a87
harinis125 <142371760+harinis125@users.noreply.github.com>
2023-11-24 12:42:18 -0800

    Update MediaLibRunner.java

--- MediaLibRunner.java
+++ MediaLibRunner.java
@@ -1,59 +1,52 @@
+// Harini Suresh
+/*
+ * Activity 2.5.7
+ * 
+ * The runner for the MediaLibrary program
+ */
 public class MediaLibRunner
 {
   public static void main(String[] args)
   {
-  System.out.println("Welcome to your Media Library");
-  MediaLib myLib = new MediaLib();
-
-
-  Book myBook = new Book("The Lord of the Rings", "Tolkien");
-  System.out.println("Book created " + myBook);
-  System.out.println("Library:\n" + myLib);
-  myLib.addBook(myBook);
-  System.out.println("Library:\n" + myLib);
-
-
-  int myRating = 5;
-  myBook.adjustRating(myRating);
-  System.out.println(myBook);
-  System.out.println("myRating=" + myRating);
-
-
-  myBook.adjustRating(15);
-  System.out.println(myBook);
-  myBook.adjustRating(-10);
-  System.out.println(myBook);
-
+    System.out.println("Welcome to your Media Library");
+    MediaLib myLib = new MediaLib();
+    Book myBook = new Book("The Lord of the Rings", "Tolkien");
+    //System.out.println("Book created " + myBook);
     
-  System.out.println("Book changed!");
-  myBook.setTitle("Fellowship of the Ring");
-  myBook.adjustRating(5); // or myBook.setAuthor("JRR Tolkien");
-  System.out.println("Book:" + myBook);
-  System.out.println("Library: " + myLib);
+    myLib.addBook(myBook);
+    //System.out.println("Library:\n" + myLib);
+
+    int myRating = 5;
+    myBook.adjustRating(myRating);
+    //System.out.println(myBook);
+
+    myBook.setTitle("Harry Potter");
+    //System.out.println("Book:" + myBook);
+    //System.out.println("Library: " + myLib);
 
 
-  System.out.println("You have a NEW Library");
-  MediaLib myLib2 = new MediaLib();
+    System.out.println("You have a NEW Library");
+    MediaLib myLib2 = new MediaLib();
 
-  Book newBook = new Book("To Kill a Mockingbird", "Lee");
-  myLib2.addBook(newBook);
-  System.out.println(myLib2);
-  newBook = new Book("1984", "Orwell");
-  System.out.println(myLib2);
+    Book newBook = new Book("To Kill a Mockingbird", "Lee");
+    myLib2.addBook(newBook);
+    //System.out.println(myLib2);
+    newBook = new Book("1984", "Orwell");
+    //System.out.println(myLib2);
 
+    // System.out.println("before test:" + newBook);
+    // myLib.testBook(newBook);
+    // System.out.println("after test:" + newBook);
 
-  /*
-  System.out.println("before test " + newBook);
-  myLib.testBook(newBook);
-  System.out.println("after test" + newBook);
-  */
+    Book newBook2 = new Book("1984", "Orwell");
+    newBook2.setAuthor("JK Rowling");
+    System.out.println(newBook + " equals " + newBook2 + ": " + newBook.equals(newBook2));
 
-
-  Book newBook2 = new Book("1984", "Orwell");
-  System.out.println(newBook2 + " equal " + newBook + "? "  + newBook2.equals(newBook));
-  newBook2.setAuthor("George Orwell");
-  System.out.println(newBook2 + " equal " + newBook + "? "  + newBook2.equals(newBook));  
-
+    // erroneous: System.out.println(newBook2.title);
+    
+    Movie newMovie = new Movie("The Lion King", 1.75);
+    myLib2.addMovie(newMovie);
+    System.out.println("Library: \n" + myLib2);
+    
   }
 }
-
