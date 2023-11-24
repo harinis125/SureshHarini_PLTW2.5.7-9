6d14970a477e897973d5c60a40ce4875753cb3e9
harinis125 <142371760+harinis125@users.noreply.github.com>
2023-11-24 12:39:45 -0800

    Update Book.java for 2.5.9

--- Book.java
+++ Book.java
@@ -1,65 +1,68 @@
+// Harini Suresh
+/*
+ * Activity 2.5.7
+ *
+ * A Book class for the MediaLibrary program
+ */
 public class Book
 {
-  private int rating;
   private String title;
   private String author;
+  private int rating;
 
   /*** Constructor ****/
   public Book(String t, String a)
   {
-  title = t;
-  author = a;
+    title = t;
+    author = a;
+    rating = 0;
   }
 
    /*** Accessor methods ***/
   public String getTitle() {
-  return title;
+    return title;
   }
 
   public String getAuthor() {
-  return author;
+    return author;
   }
 
   public int getRating() {
-  return rating;
+    return rating;
   }
 
-  public String toString()  
+  public String toString() 
   {
-  String info = "\"" + title + "\", written by " + author;
-  if (rating != 0)  
-  info += ", rating is " + rating;
-
-  return info;
+    String info = "\"" + title + "\", written by " + author;
+    if (rating != 0) 
+    { 
+      info += ", rating is " + rating;
+    }
+    return info;
   }
 
-
-  public boolean equals(Book b)
-  {
-  if(this.title.equals(b.title) && this.author.equals(b.author))
-    return true;  
-  return false;
-
+  public boolean equals(Book b) {
+    if(this.title.equals(b.title) && this.author.equals(b.author)) {
+      return true;
+    } 
+    return false;
   }
 
   /*** Mutator methods ***/
-  public void setAuthor(String a) {
-  author = a;
-  }
-
   public void setTitle(String t) {
-  title = t;
+    title = t;
   }
 
+  public void setAuthor(String a) {
+    author = a;
+  }
   
-  public void adjustRating(int r)
-  {
-
-  if ((rating + r >= 0) && (rating + r <= 10))
+  public int adjustRating(int r) {
+    System.out.println("Adjusting rating by " + r);
     rating += r;
-
-  r = 10;
-
+    if (rating > 10 || rating < 0) {
+      rating -= r;
   }
-
+    return rating;
+}
 }
